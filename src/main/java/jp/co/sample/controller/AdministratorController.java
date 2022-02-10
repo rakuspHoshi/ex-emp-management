581dff4cf614db11bcc90b5e034e9a91f2140142
rakuspHoshi <ryuto.hoshi@rakus-partners.co.jp>
2022-02-10 14:02:35 +0900

    インクルード化

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -66,5 +66,11 @@ public class AdministratorController {
 			return "forward:/employee/showList";
 		}
 	}
+	
+	@RequestMapping("/logout")
+	public String logout() {
+		session.invalidate();
+		return "redirect:/";
+	}
 
 }
